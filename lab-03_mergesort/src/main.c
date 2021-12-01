46867e26b1c1f85de98117ed3da5bd865887f3b4
MarkPermyak <mark.permyakov.2002@bk.ru>
2021-12-02 00:22:00 +0300

    Fixed empty cases

--- lab-03_mergesort/src/main.c
+++ lab-03_mergesort/src/main.c
@@ -18,6 +18,9 @@ int main(int argc, char** argv){
         size_t element_size = sizeof(int);
         comp_t comparator = int_gt_comparator;
         
+        if (argc == 2)
+            printf("\n");
+        else{
         int array[argc-2];
         for (int i = 2; i < argc; i++)
         {
@@ -27,11 +30,14 @@ int main(int argc, char** argv){
         printf("%d", array[argc-3]);
         for (int i =0; i<argc-3; ++i)
             printf(" %d", array[argc - 4 - i]);
-                
+        }   
     }    
     if (strcmp(argv[1], "char") == 0){
         size_t element_size = sizeof(char);
         comp_t comparator = char_gt_comparator;
+        if (argc == 2)
+            printf("\n");
+        else{
         char array[argc-2];
         for (int i = 2; i < argc; i++)
         {
@@ -42,13 +48,16 @@ int main(int argc, char** argv){
         for (int i =0; i<argc-3; ++i)
             printf(" %c", array[argc - 4 - i]);
         printf("\n");
+        }
     } 
  
-    // if (strcmp(argv[1], "str") == 0){
-    //     element_size = sizeof(char *);
-    //     comparator = strcmp;
-    //     type = "str";
-    // } 
+    if (strcmp(argv[1], "str") == 0){
+        if (argc == 2)
+            printf("\n");
+        else{
+
+        }
+    } 
 
 
     return 0;
