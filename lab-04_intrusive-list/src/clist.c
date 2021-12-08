92f8cd33d5d3887ca9ed7683eb678a9e5302618c
MarkPermyak <mark.permyakov.2002@bk.ru>
2021-12-08 21:56:00 +0300

    Fixed some codestyle and minor issues

--- lab-04_intrusive-list/src/clist.c
+++ lab-04_intrusive-list/src/clist.c
@@ -15,78 +15,53 @@ void apply(intrusive_list_t* l, void (*func)(intrusive_node_t*)) {
 
     while (cur) {
         intrusive_node_t* next = cur->next;
-
         func(cur);
         //printf("%s\n", "OKapply");
 
-        cur = next;
-    }
-}
-
-void add_node(intrusive_list_t *l, point_t* new) {
-    
-
-    
-    intrusive_node_t* first = l->head.next;
-
-    l->head.next = &new->node;
-    
-
-    new->node.prev = &l->head;
-    new->node.next = first;
-    if(first)
-        first->prev = &new->node;
-
-}
-
-void remove_point(intrusive_list_t* l, point_t* new){
-    intrusive_node_t* cur = l->head.next;
-    int x = new->x;
-    int y = new->y;
-   
-    while (cur){
-        point_t* cur_point = get_point(cur);
-        
-        if(cur_point->x == x && cur_point->y == y){
-            cur->prev->next = cur->next;
-            if(cur->next)
-                cur->next->prev = cur->prev;
-        }
-        
         cur = cur->next;
     }
 }
 
-point_t* get_point(intrusive_node_t* node_ptr) {
-    return container_of(node_ptr, point_t, node);
+void add_node(intrusive_list_t *l, intrusive_node_t* new) {
+        
+    intrusive_node_t* first = l->head.next;
+
+    l->head.next = new;
+    //printf("%s\n", "OK");
+
+    new->prev = &l->head;
+    new->next = first;
+    if(first)
+        first->prev = new;
+    //printf("%s\n", "OK");
 }
 
+void remove_node(intrusive_list_t* l, intrusive_node_t* del){
+    
+    if(del->prev)
+        del->prev->next = del->next;
+    
+    if(del->next)
+        del->next->prev = del->prev;
+        
+    del = NULL;
+}
+
+void remove_all_nodes(intrusive_list_t *l){
+    intrusive_node_t* cur = &l->head;
+
+    while (cur->next)
+        cur = cur->next;
+    
+    while (cur){
+        remove_node(l, cur);
+        cur = cur->prev;
+    }
+    
+    
+}
 void free_node(intrusive_node_t* node) {
-    free(get_point(node));
-}
-
-void remove_all_points(intrusive_list_t *l) {
-    apply(l, free_node);
-    init_list(l);
-}
-
-void print_point(intrusive_node_t* node) {
-    point_t* point = get_point(node);
-
-    printf("(%d %d) ", point->x, point->y);
-}
-
-void show_all_points(intrusive_list_t l){
-    apply(&l, print_point);
-    printf("\n");
-}
-point_t* alloc_point(int x, int y){
-    intrusive_node_t* node = malloc(sizeof(intrusive_node_t));
-    point_t* point = malloc(sizeof(point_t));
-    point->node = *node;
-    point->x = x;
-    point->y = y;
-    return point;
+    node = NULL;
 }
 
 int get_length(intrusive_list_t* l){
@@ -98,4 +73,9 @@ int get_length(intrusive_list_t* l){
     }
 
     return length;
+}
+
+void test(intrusive_list_t* l){
+    intrusive_node_t* cur = l->head.next;
+    printf("%d %d\n", get_point(cur)->x, get_point(cur)->y);
 }
--- lab-04_intrusive-list/src/main.c
+++ lab-04_intrusive-list/src/main.c
@@ -6,16 +6,48 @@
 
 #define LINESIZE 239
 
-
-// /* ? */ add_point(intrusive_list *l, int x, int y) {
-//     point A;
+void add_point(intrusive_list_t* l, int x, int y){
+    point_t* point = malloc(sizeof(point_t));
+    intrusive_node_t* node = malloc(sizeof(intrusive_node_t));
+    add_node(l, &point->node);
     
-// }
+    point->x = x;
+    point->y = y;
+}
 
-// /* ? */ remove_point(intrusive_list /* ? */, int x, int y) { // removes all (x, y) pairs
+void remove_point(intrusive_list_t* l, int x, int y){
 
-// }
+    intrusive_node_t* cur = l->head.next;
 
+    while (cur){
+      point_t* cur_point = get_point(cur);
+
+      if (cur_point->x == x && cur_point->y == y){
+        remove_node(l, cur);
+        cur_point = NULL;
+      }
+      cur = cur->next;
+    }
+}
+
+void remove_all_points(intrusive_list_t l) {
+    remove_all_nodes(&l);
+    init_list(&l);
+}
+
+void print_point(intrusive_node_t* node) {
+    point_t* point = get_point(node);
+    printf("(%d %d) ", point->x, point->y);
+}
+
+void show_all_points(intrusive_list_t l){
+    apply(&l, print_point);
+    printf("\n");
+}
+
+point_t* get_point(intrusive_node_t* node_ptr) {
+    return container_of(node_ptr, point_t, node);
+}
 
 int main() {
 
@@ -28,78 +60,56 @@ int main() {
     fgets(input, LINESIZE, stdin);
     input[strlen(input) - 1] = '\0';
     
-
-    if(input[0] == 'a' && input[1] == 'd' && input[2] == 'd' && input[3] == ' '){
-      char num1[LINESIZE] = "";
-      int i = 0;
-      while(!isdigit((unsigned char)input[i]) && (input[i] != '-' ))
-        i++;
-      int j = 0;
-
-      while(input[i] != ' '){
-        num1[j] = input[i];
-        j++;
-        i++;
-      }
-      int x = atoi(num1);
-      char num2[LINESIZE] = "";
-      i++;
-      int k = 0;
-      while(input[i]){
-        num2[k] = input[i];
-        i++;
-        k++;
-      }
-      int y = atoi(num2);
-     
-      add_node(&my_list, alloc_point(x, y));  
+    if (!strcmp(input, "exit")){
+      break;
+      return 0;
     }
     else{
-      if(!strcmp(input, "rma")){
-        remove_all_points(&my_list);
-      }
-      else{
-        if(!strcmp(input, "print")){
-          show_all_points(my_list);
-        }
-
-        else{  
-          if(!strcmp(input, "len"))
+      if (!strcmp(input, "len"))
             printf("%d\n", get_length(&my_list));
+      else{
+        if (!strcmp(input, "print"))
+          show_all_points(my_list);
+        else{
+          if (!strcmp(input, "rma"))
+            remove_all_points(my_list);
           else{
-            if(!strcmp(input, "exit")){
-              break;
-            }
-            else{
-              if (input[0] == 'r' && input[1] == 'm' && input[2] == ' '){
-                char num1[LINESIZE] = "";
-                int i = 0;
-                while(!isdigit((unsigned char)input[i]) && (input[i] != '-' ))
-                  i++;
-                int j = 0;
-
-                while(input[i] != ' '){
-                  num1[j] = input[i];
-                  j++;
-                  i++;
-                }
-                int x = atoi(num1);
-                char num2[LINESIZE] = "";
+            
+              char num1[LINESIZE] = "";
+              int i = 0;
+              while(!isdigit((unsigned char)input[i]) && (input[i] != '-' ))
                 i++;
-                int k = 0;
-                while(input[i]){
-                  num2[k] = input[i];
-                  i++;
-                  k++;
-                }
-                int y = atoi(num2);
-                remove_point(&my_list, alloc_point(x, y));
+              int j = 0;
+
+              while(input[i] != ' '){
+                num1[j] = input[i];
+                j++;
+                i++;
               }
-              else
-                printf("%s\n", "Unknown command");
-            }
+              int x = atoi(num1);
+              char num2[LINESIZE] = "";
+              i++;
+              int k = 0;
+              while(input[i]){
+                num2[k] = input[i];
+                i++;
+                k++;
+              }
+              int y = atoi(num2);
+
+              if(input[0] == 'a' && input[1] == 'd' && input[2] == 'd' && input[3] == ' ')
+                add_point(&my_list, x, y);  
+              else{
+                if(input[0] == 'r' && input[1] == 'm' && input[2] == ' ')
+                  remove_point(&my_list, x, y);
+                else
+                  printf("%s\n", "Unknown command");
+              }
+            
+
           }
-        }  
+          
+        }
       }
     }
     fflush (stdout);
