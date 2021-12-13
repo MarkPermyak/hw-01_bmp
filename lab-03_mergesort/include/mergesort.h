ea37c2b5a39df641a84cf51b00a5da93f302021e
MarkPermyak <mark.permyakov.2002@bk.ru>
2021-12-13 14:21:40 +0300

    Minor issues

--- lab-03_mergesort/include/mergesort.h
+++ lab-03_mergesort/include/mergesort.h
@@ -5,7 +5,9 @@
 
 typedef int (*comp_t)(const void *, const void *);
 
-//typedef void (*print_t)(const void*);
+typedef void (*fill_t)( void*, void**, int);
+
+typedef void (*print_t)(void*, int);
 
 void mergesort(void *array, size_t elements, size_t element_size, 
                 int (*comparator)(const void *, const void *));
--- lab-03_mergesort/src/main.c
+++ lab-03_mergesort/src/main.c
@@ -17,6 +17,18 @@ int str_gt_comparator(const void *s1, const void *s2){
     return strcmp(*(char**)s1, *(char**)s2);
 }
 
+int* create_int_array(int array_size, int element_size){
+    return malloc(array_size * element_size);
+}
+
+char* create_char_array(int array_size, int element_size){
+    return malloc(array_size * element_size);
+}
+
+char** create_str_array(int array_size, int element_size){
+    return malloc(array_size * element_size);
+}
+
 void fill_int_array(int *array, char **argv, int array_size){
     for (int i = 0; i < array_size; i++)
         array[i] = atoi(argv[i+2]);
@@ -31,6 +43,7 @@ void fill_str_array(char **array, char **argv, int array_size){
     for (int i = 0; i < array_size; i++)
         array[i] = argv[i + 2];
 }
+
 void int_printer(int *array, int array_size){
     printf("%d", array[array_size - 1]);
     for (int i =0; i<array_size - 1; ++i)
@@ -59,59 +72,51 @@ int main(int argc, char** argv){
             printf("\n");
     
     else{
-    
-    int array_size = argc - 2;
-    
-    if (!strcmp(argv[1], "int")){
-        
+
+        int array_size = argc - 2;
+
+        if (!strcmp(argv[1], "int")){
+
             size_t element_size = sizeof(int);
             comp_t comparator = int_gt_comparator;
             
-            int* array = malloc(array_size * element_size);
+            int* array = create_int_array(array_size, element_size);
 
             fill_int_array(array, argv, array_size);
-            
             mergesort(array, array_size, element_size, comparator);
-            
             int_printer(array, array_size);
 
             free(array);
-    }    
+        }    
 
-    if (strcmp(argv[1], "char") == 0){
-        
+        if (strcmp(argv[1], "char") == 0){
+            
             size_t element_size = sizeof(char);
             comp_t comparator = char_gt_comparator;
-              
-            char* array = malloc(array_size * element_size);
+                
+            char* array = create_char_array(array_size, element_size);
 
             fill_char_array(array, argv, array_size);
-
             mergesort(array, array_size, element_size, comparator);
-            
             char_printer(array, array_size);
 
             free(array);
-    } 
- 
-    if (strcmp(argv[1], "str") == 0){
-        
+        } 
 
-     
+        if (strcmp(argv[1], "str") == 0){
+            
             size_t element_size = sizeof(char *);
             comp_t comparator = str_gt_comparator;
 
-            char** array = malloc((array_size) * sizeof(char*));
+            char** array = create_str_array(array_size, element_size);
             
             fill_str_array(array, argv, array_size);
-            
             mergesort(array, array_size, element_size, comparator);
-            
             str_printer(array, array_size);
 
             free(array);
-        
-    }
+            
+        }
     }
 
     return 0;
