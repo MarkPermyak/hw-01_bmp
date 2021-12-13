50dc707c9ecfee7353a7080311d0148a095ad60e
MarkPermyak <mark.permyakov.2002@bk.ru>
2021-12-13 13:29:22 +0300

    Added fill functions

--- lab-03_mergesort/src/main.c
+++ lab-03_mergesort/src/main.c
@@ -17,6 +17,20 @@ int str_gt_comparator(const void *s1, const void *s2){
     return strcmp(*(char**)s1, *(char**)s2);
 }
 
+void fill_int_array(int *array, char **argv, int array_size){
+    for (int i = 0; i < array_size; i++)
+        array[i] = atoi(argv[i+2]);
+}
+
+void fill_char_array(char *array, char **argv, int array_size){
+    for (int i = 0; i < array_size; i++)
+        array[i] = *argv[i+2];
+}
+
+void fill_str_array(char **array, char **argv, int array_size){
+    for (int i = 0; i < array_size; i++)
+        array[i] = argv[i + 2];
+}
 void int_printer(int *array, int array_size){
     printf("%d", array[array_size - 1]);
     for (int i =0; i<array_size - 1; ++i)
@@ -41,69 +55,64 @@ void str_printer(char **array, int array_size){
 }
 
 int main(int argc, char** argv){
+    if (argc == 2)
+            printf("\n");
+    
+    else{
+    
+    int array_size = argc - 2;
+    
     if (!strcmp(argv[1], "int")){
         
-        if (argc == 2)
-            printf("\n");
-
-        else{
             size_t element_size = sizeof(int);
             comp_t comparator = int_gt_comparator;
-            int array_size = argc - 2;
-            int array[array_size];
             
-            for (int i = 2; i < argc; i++)
-                array[i-2] = atoi(argv[i]);
+            int array = malloc(array_size * element_size);
+
+            fill_int_array(array, argv, array_size);
             
             mergesort(array, array_size, element_size, comparator);
             
             int_printer(array, array_size);
-        }   
+
+            free(array);
     }    
 
     if (strcmp(argv[1], "char") == 0){
         
-        if (argc == 2)
-            printf("\n");
-        
-        else{
             size_t element_size = sizeof(char);
             comp_t comparator = char_gt_comparator;
-           
-            int array_size = argc - 2;    
-            char array[array_size];
-           
-            for (int i = 2; i < argc; i++)
-                array[i-2] = *argv[i];
+              
+            char array = malloc(array_size * element_size);
+
+            fill_char_array(array, argv, array_size);
 
             mergesort(array, array_size, element_size, comparator);
             
             char_printer(array, array_size);
-        }
+
+            free(array);
     } 
  
     if (strcmp(argv[1], "str") == 0){
-        if (argc == 2)
-            printf("\n");
+        
 
-        else{
+     
             size_t element_size = sizeof(char *);
             comp_t comparator = str_gt_comparator;
 
-            int array_size = argc - 2;
             char** array = malloc((array_size) * sizeof(char*));
             
-            for (int i = 0; i < array_size; i++)
-                array[i] = argv[i + 2];
+            fill_str_array(array, argv, array_size);
             
             mergesort(array, array_size, element_size, comparator);
             
             str_printer(array, array_size);
 
             free(array);
-        }
-    } 
-
+        
+    }
+    }
 
     return 0;
 }
