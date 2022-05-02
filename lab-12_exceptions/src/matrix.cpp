2ad0ceba4150e06a54c9c9ce44521396074bea49
MarkPermyak <mark.permyakov.2002@bk.ru>
2022-05-02 23:43:47 +0300

    fixed mul mem limit v2

--- lab-12_exceptions/src/matrix.cpp
+++ lab-12_exceptions/src/matrix.cpp
@@ -123,24 +123,19 @@ Matrix& Matrix::operator-=(const Matrix& m) {
     return *this;
 }
 
-Matrix& Matrix::operator*=(const Matrix& m) {
-    if( _cols != m._rows)
+Matrix Matrix::operator*(const Matrix &matrix) const {
+    if (_cols != matrix._rows)
         throw MatrixException("MUL: #arg1.columns != #arg2.rows.");
 
-    Matrix mult(_rows, m._cols);
+    Matrix matrix_result(_rows, matrix._cols);
 
-    for(std::size_t i = 0; i < _rows; i++)
-        for(std::size_t j = 0; j < m._cols; j++)
-            for(std::size_t k = 0; k < _cols; k++)
-                mult._data[i][j] += (long long)_data[i][k]*m._data[k][j];    
+    for (std::size_t i = 0; i < matrix_result._rows; i++)
+        for (std::size_t j = 0; j < matrix_result._cols; j++)
+            for (std::size_t k = 0; k < _cols; k++)
+                matrix_result._data[i][j] += _data[i][k] * matrix._data[k][j];
 
-    _cols = m._cols;
-
-    *this = mult;
-
-    return *this;
+    return matrix_result;
 }
-
 Matrix Matrix::operator+(const Matrix& m) const {
     Matrix tmp(*this);
     tmp += m;
@@ -153,10 +148,9 @@ Matrix Matrix::operator-(const Matrix& m) const {
     return tmp;
 }
 
-Matrix Matrix::operator*(const Matrix& m) const {
-    Matrix tmp(*this);
-    tmp *= m;
-    return tmp;
+Matrix &Matrix::operator*=(const Matrix &matrix) {
+    *this = *this * matrix;
+    return *this;
 }
 
 Matrix read_matrix_from_file(std::string filename){
