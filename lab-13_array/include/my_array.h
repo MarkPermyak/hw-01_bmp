bf902c2b1fce27f1d3369a21c9adcb25f836f02e
MarkPermyak <mark.permyakov.2002@bk.ru>
2022-06-09 15:11:16 +0300

    bool array

--- lab-13_array/include/my_array.h
+++ lab-13_array/include/my_array.h
@@ -46,6 +46,89 @@ private:
 	T _data[N]{};
 };
 
+bool get_bit(uint8_t value, std::size_t index) {
+	return (value >> index) & 1;
+}
+
+void set_bit(uint8_t& value, std::size_t index, bool new_bit) {
+	if (get_bit(value, index) != new_bit)
+		value ^= 1 << index;
+}
+
+template<std::size_t N>
+class my_array<bool, N> {
+public:
+
+	std::size_t size() const {
+		return N; 
+	}
+
+	bool empty() const {
+		return size() == 0;
+	}
+
+	class bool_reference {
+	public:
+		bool_reference(uint8_t& block_value, std::size_t position_in_block) : 
+			block_value(block_value), 
+			position_in_block(position_in_block) {}
+
+		operator bool() const {
+			return get_bit(block_value, position_in_block);
+		}
+
+		bool_reference& operator=(bool new_bit) {
+			set_bit(block_value, position_in_block, new_bit);
+			return *this;
+		}
+
+		bool_reference& operator=(bool_reference& source) {
+			return *this = (bool)source;
+		}
+
+	private:
+		uint8_t& block_value;
+		std::size_t position_in_block; 
+	};
+
+	bool_reference operator[](std::size_t index) {
+		return bool_reference(_data[get_block_id(index)], get_position_in_block(index));
+	}
+
+	bool operator[](std::size_t index) const {
+		return get_bit(_data[get_block_id(index)], get_position_in_block(index));
+	}
+
+	bool_reference at(std::size_t index) {
+		return bool_reference(_data.at(get_block_id(index)), get_position_in_block(index));
+	}
+
+	bool at(std::size_t index) const {
+		return get_bit(_data.at(get_block_id(index)), get_position_in_block(index)); 
+	}
+
+	void fill(bool value) {
+		if (value)
+			_data.fill((1 << BITS_IN_BLOCK) - 1);
+		else
+			_data.fill(0);
+	}
+
+private:
+	const std::size_t BITS_IN_BLOCK = 8;
+
+	std::size_t get_block_id(std::size_t index) {
+		return index / BITS_IN_BLOCK;
+	}
+
+	std::size_t get_position_in_block(std::size_t index) {
+		return index % BITS_IN_BLOCK;
+	}
+
+	//minimum x such that BITS_IN_BLOCK * x >= N
+	my_array<uint8_t, (N + BITS_IN_BLOCK - 1) / BITS_IN_BLOCK> _data;  	
+};
+
 }
 
 #endif  // MY_ARRAY_H_
